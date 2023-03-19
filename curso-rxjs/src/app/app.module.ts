f35d9a600003ac39f19a13a69ff2965eddbdf85e
Larissa Silva Novaes <larissanovaes@brq.com>
2023-03-18 21:37:39 -0300

    adicionando estrutura inicial

---
+++ curso-rxjs/src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ObservablesComponent } from './componentes/observables/observables.component';
+import { ForkjoinComponent } from './componentes/operadores-criacao-juncao/forkjoin/forkjoin.component';
+import { AjaxComponent } from './componentes/operadores-criacao/ajax/ajax.component';
+import { FromEventComponent } from './componentes/operadores-criacao/from-event/from-event.component';
+import { FromComponent } from './componentes/operadores-criacao/from/from.component';
+import { IntervalComponent } from './componentes/operadores-criacao/interval/interval.component';
+import { OfComponent } from './componentes/operadores-criacao/of/of.component';
+import { ThrowErrorComponent } from './componentes/operadores-criacao/throw-error/throw-error.component';
+import { TimerComponent } from './componentes/operadores-criacao/timer/timer.component';
+import { SubjectComponent } from './componentes/subject/subject.component';
+import { ZipComponent } from './componentes/operadores-criacao-juncao/zip/zip.component';
+import { MergeConcatComponent } from './componentes/operadores-criacao-juncao/merge-concat/merge-concat.component';
+import { MapComponent } from './componentes/operadores-transformacao/map/map.component';
+import { MapToComponent } from './componentes/operadores-transformacao/map-to/map-to.component';
+import { SwitchMapComponent } from './componentes/operadores-transformacao/switch-map/switch-map.component';
+import { PluckComponent } from './componentes/operadores-transformacao/pluck/pluck.component';
+import { ToArrayComponent } from './componentes/operadores-transformacao/to-array/to-array.component';
+import { FilterComponent } from './componentes/operadores-filtragem/filter/filter.component';
+import { TakeComponent } from './componentes/operadores-filtragem/take/take.component';
+import { TakeUntilComponent } from './componentes/operadores-filtragem/take-until/take-until.component';
+import { TakeWhileComponent } from './componentes/operadores-filtragem/take-while/take-while.component';
+import { SkipComponent } from './componentes/operadores-filtragem/skip/skip.component';
+import { DebounceTimeComponent } from './componentes/operadores-filtragem/debounce-time/debounce-time.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ObservablesComponent,
+    SubjectComponent,
+    FromComponent,
+    OfComponent,
+    FromEventComponent,
+    IntervalComponent,
+    TimerComponent,
+    ThrowErrorComponent,
+    AjaxComponent,
+    ForkjoinComponent,
+    ZipComponent,
+    MergeConcatComponent,
+    MapComponent,
+    MapToComponent,
+    SwitchMapComponent,
+    PluckComponent,
+    ToArrayComponent,
+    FilterComponent,
+    TakeComponent,
+    TakeUntilComponent,
+    TakeWhileComponent,
+    SkipComponent,
+    DebounceTimeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
